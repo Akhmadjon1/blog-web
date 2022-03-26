ffe08de0594e8373986480d924c4cca74a42c7df
Akhmadjon Zukhriddinov <akhmadjonzukhriddinov@Akhmadjonui-MacBookPro.local>
2022-03-27 00:28:11 +0900

    add new stuff

--- app.js
+++ app.js
@@ -50,6 +50,19 @@ app.post("/compose", function(req, res) {
 
 });
 
+app.get("/posts/:postName", function(req, res) {
+  const requestedTitle = req.params.postName;
+
+  posts.forEach(function(post) {
+    const storedTitle = post.title;
+  
+    if (storedTitle === requestedTitle) {
+      console.log("Match found");
+    }
+  });
+
+});
+
 
 
 
