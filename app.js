dcd68c847628210726dd2d7bb019185009133a6f
Akhmadjon Zukhriddinov <akhmadjonzukhriddinov@Akhmadjonui-MacBookPro.local>
2022-04-22 22:47:14 +0900

    complete to connect to mongodb

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const _ = require("lodash");
 
-const homeStartingContent = "What is a personal blog website🤔? The personal blog is an ongoing online diary or commentary written by an individual, rather than a corporation or organization.😃";
+const homeStartingContent = "What is a personal blog website🤔? The personal blog is an ongoing online diary or commentary written by an individual, rather than a corporation or organization😃";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
@@ -55,31 +55,26 @@ app.post("/compose", function(req, res) {
     content: req.body.postBody
   });
 
-  posts.push(post);
 
   post.save(function(err){
     if(!err){
       res.redirect("/",);
     }
   });
-
-
 });
 
-app.get("/posts/:postName", function(req, res) {
-  const requestedTitle = _.lowerCase(req.params.postName);
+app.get("/posts/:postId", function(req, res) {
+  const requestedTitle = _.lowerCase(req.params.postId);
 
-  posts.forEach(function(post) {
-    const storedTitle = _.lowerCase(post.title);
+  const requestedPostId = req.params.postId;
+
   
-    if (storedTitle === requestedTitle) {
+    Post.findOne({_id: requestedPostId}, function(err, post) {
         res.render("post", {
           title: post.title,
           content: post.content
         });
-    };
   });
-
 });
 
 
