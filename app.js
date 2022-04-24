a939d24eb6680c82c036c2e62289be35ec164a14
Akhmadjon Zukhriddinov <akhmadjonzukhriddinov@Akhmadjonui-MacBookPro.local>
2022-04-24 18:40:34 +0900

    add gitignore, procfile and update ports

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://akhmadjon:test-123@cluster0.oahlw.mongodb.net/blogDB", {useNewUrlParser: true});
 
 const postSchema = {
   title: String,
@@ -78,8 +78,11 @@ app.get("/posts/:postId", function(req, res) {
 });
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server has started Successfully");
 });
